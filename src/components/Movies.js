b39e70647d0e8a5aa92130b385361f8e7bb8fc4d
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-23 18:46:59 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie, index) => {
+          return <div key={index}>
+            <h4>Title: {movie.title}</h4>
+            <h5>Time: {movie.time}</h5>
+            <ul> 
+              {movie.genres.map((genre, index) => {
+                return <li key={index}>{genre}</li>
+              })}
+            </ul>
+          </div>
+        })}
     </div>
   );
 };
